392ea000a3f062b867a8e6e5942fbd42e60ae339
durdona <456@.ru>
2023-09-22 13:45:06 +0500

    a

---
+++ app.js
@@ -0,0 +1,113 @@
+/* //Задание 1
+
+// let age = +prompt('Bведите возраст')
+
+// if(age <=18 ){
+//    alert('Вы еще молоды, Вам нужно учиться')
+// }else if(age >=18 && age <=50 ){
+//    alert('Вам нужно работать')
+// }else if(age >=50 && age <=59 ){
+//    alert('Вам скоро на пенсию')
+// }else if(age >=59 && age <=100 ){
+//    alert('Вы пенсионер')
+// }else{
+//    alert('Что-то пошло не так')
+// } */
+
+
+
+
+// /* Задание 2
+
+// let time = +prompt('Введите время')
+ 
+// let hours1 = ' часов '
+// let hours2 = ' часа '
+
+// let night = ' ночи '
+// let morning = ' утра '
+// let afternoon = ' дня '
+// let evening = ' вечера '
+
+// switch(time){
+
+//    case 0:
+//       alert(12 + hours1 + night)
+//       break
+//    case 1:
+//       alert(' час ' + night)
+//    case 2:
+//    case 3:
+//    case 4:
+//       alert(time + hours1 + night)
+//       break
+//    case 5:
+//    case 6:
+//    case 7:
+//    case 8:
+//    case 9:
+//    case 10:
+//    case 11:
+//       alert (time + hours1 + morning)
+//       break
+//    case 12:
+//       alert (time + hours1 + afternoon)
+//       break
+//    case 13:
+//       alert (' час ' + afternoon)
+//    case 14:
+//       alert (2 + hours2 + afternoon)
+//       break
+//    case 15:
+//       alert (3 + hours2 + afternoon)
+//       break
+//    case 16:
+//       alert (4 + hours1 + afternoon)
+//       break
+//    case 17:
+//       alert (5 + hours1 + afternoon)
+//       break
+//    case 18:
+//       alert (6 + hours1 + evening)
+//       break
+//    case 19:
+//       alert (7 + hours1 + evening)
+//       break
+//    case 20:
+//       alert (8 + hours1 + evening)
+//       break
+//    case 21:
+//       alert (9 + hours1 + evening)
+//       break
+//    case 22:
+//       alert (10 + hours1 + evening)
+//       break
+//    case 23:
+//       alert (11 + hours1 + evening)
+//       break 
+//    default:
+//       alert ('Такого времени нет')
+// } */
+
+
+
+/* //Задание 3 */
+/* let x = +prompt('Введите первое число')
+let y = +prompt('Введите второе число')
+let z = +prompt('Введите третье число')
+if(x<y && x>z || x>y && x<z){
+
+   alert('Cреднее число ' + x)
+   
+
+}else if(y<x && y>z || y>x && y<z){
+   alert('Cреднее число ' + y)
+   
+
+}else if(z<x && z>y || z>x && z<y){
+   alert('Cреднее число ' + z)
+   
+} */
+
+
+
